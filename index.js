b630c21f547774fe706dcd862e29e7a644d58f76
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-25 03:37:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ dodger.style.bottom = "0px";
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", ""); // gets rid of px
-    let left = parseInt(leftNumbers, 10); // gets rid of ""
-  };
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
   dodger.style.left = `${left - 1}px`;
+  }
 })
