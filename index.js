c09015be64fb50b199879e1d17cae94c5f7e8c6d
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-25 03:25:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ let dodger = document.getElementById('dodger');
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.left;
 dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function(e) {
+  console.log(e.key);
+})
