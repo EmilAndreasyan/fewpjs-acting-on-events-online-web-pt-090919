b2b47ff905fbf62efb6f21f21317ea875d4e206f
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-25 04:00:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ dodger.style.bottom = "0px";
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
-    let leftNumbers = dodger.style.left.replace("px", "");
-    let left = parseInt(leftNumbers, 10);
-  dodger.style.left = `${left - 1}px`;
+    moveDodgerLeft();
+  } if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
 
@@ -17,5 +17,13 @@ function moveDodgerLeft () {
   let left = parseInt(leftNumbers, 10);
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "")
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
   }
 }
